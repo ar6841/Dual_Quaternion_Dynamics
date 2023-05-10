4c2920857d486e06efb829140e5c6fbd63eb5b6f
Arjun Raja <ar6841@nyu.edu>
2023-05-10 12:52:43 -0400

    Added forces and momentum

--- Dynamics_common.h
+++ Dynamics_common.h
@@ -1,6 +1,6 @@
 #pragma once
 /*
-This header file contains common functions for creating the dual intertia matrix, 
+This header file contains common functions for creating the dual intertia matrix, dual momentum vector
 */
 
 #include "Eigen/Core"
@@ -10,7 +10,7 @@ This header file contains common functions for creating the dual intertia matrix
 namespace Dynamics
 {
 template<typename T>
-Eigen::Matrix<T,8,8> //TODO: Make this sparse matrix
+Eigen::Matrix<T,8,8> // TODO: Make this a sparse matrix
 DualInertia(const T& mass, const Eigen::Matrix<T,3,3>& inertia_3x3)
 {
     T zero = T(0);
@@ -19,24 +19,33 @@ DualInertia(const T& mass, const Eigen::Matrix<T,3,3>& inertia_3x3)
                                     zero, zero, mass, zero, zero, zero, zero, zero
                                     zero, zero, zero, mass, zero, zero, zero, zero
                                     zero, zero, zero, zero, T(1), zero, zero, zero
-                                    zero, zero, zero, zero, zero, inertia(1,1), inertia(1,2), inertia(1,3)
-                                    zero, zero, zero, zero, zero, inertia(2,1), inertia(2,2), inertia(3,3)
-                                    zero, zero, zero, zero, zero, inertia(3,1), inertia(3,2), inertia(3,3)).finished();
+                                    zero, zero, zero, zero, zero, inertia(0,0), inertia(0,1), inertia(0,2)
+                                    zero, zero, zero, zero, zero, inertia(1,0), inertia(1,1), inertia(1,2)
+                                    zero, zero, zero, zero, zero, inertia(2,0), inertia(2,1), inertia(2,2)).finished();
 }
 
 template<typename T>
 Eigen::Matrix<T,8,1>
 DualMomentum(const Eigen::Matrix<T,8,8>& DualIntertia, const dualquat::DualQuaternion<T>& twist)
 {
-    return (DualInertia*dualquat_to_vec(twist)).finished();
+    return (DualInertia*dualquat_swap(twist)).finished();
 }
 
 template<typename T>
 Eigen::Matrix<T,8,1>
 DualMomentum(const Eigen::Matrix<T,8,8>& DualIntertia, const Eigen::Matrix<T,8,1>& twist)
 {
-    return (DualInertia*twist).finished();
+    return (DualInertia*(Eigen::Matrix<T,8,1>(twist(4,1),
+                                            twist(5,1),
+                                            twist(6,1),
+                                            twist(7,1),
+                                            twist(0,1),
+                                            twist(1,1),
+                                            twist(2,1),
+                                            twist(3,1)).finished())).finished();
 }
 
 
+/*
+*/
 } // namespace Dynamics
---
+++ forces.h
@@ -0,0 +1,57 @@
+#pragma once
+/*
+This header file contains common functions for modelling forces in dual quaternion space
+*/
+
+#include "Eigen/Core"
+#include "Eigen/Geometry"
+#include "dualquat/dualquat_base.h"
+
+namespace Dynamics
+{
+
+template<typename T>
+void Check_ERROR_DUALQUATPURE(const std::string& error, const dualquat::DualQuaternion<T> dq, const T& tol)
+{
+    if(!dualquat::is_pure(dq,tol))
+    {
+        throw std::invalid_argument(("\n Not pure imaginary dual quaternion at : " + error));
+    }
+}
+
+/*
+Generalized wrench given a force and a moment
+*/
+template<typename T>
+dualquat::DualQuaternion<T> 
+Wrench(const Eigen::Vector3<T>& force, const Eigen::Vector3<T>& moment)
+{
+    return dualquat::DualQuaternion(force,moment);
+}
+
+/*
+Generalized wrench given a force and a moment
+*/
+template<typename T>
+dualquat::DualQuaternion<T> 
+Wrench(const Eigen::Quaternion<T>& force, const Eigen::Quaternion<T>& moment)
+{
+    Check_ERROR_DUALQUATPURE("Wrench()",dualquat::DualQuaternion(force,moment),T(0.0001));
+    return dualquat::DualQuaternion(force,moment);
+}
+
+/*
+Generalized wrench at some point given a force and a moment
+*/
+template<typename T>
+dualquat::DualQuaternion<T> 
+Wrench(const Eigen::Vector3<T>& force,  const Eigen::Vector3<T>& moment, const Eigen::Vector3<T>& point)
+{
+    return dualquat::DualQuaternion(force,(moment+point.cross(force)));
+}
+
+
+
+
+
+} // namespace Dynamics
