b6d33b4250cf10e0e9345d1ce5e19b8da765eb30
Arjun Raja <ar6841@nyu.edu>
2023-05-11 00:49:28 -0400

    Update forces.h

--- forces.h
+++ forces.h
@@ -50,4 +50,5 @@ Wrench(const Eigen::Vector3<T>& force,  const Eigen::Vector3<T>& moment, const E
     return dualquat::DualQuaternion(force,(moment+point.cross(force)));
 }
 
+
 } // namespace Dynamics
